19366e1f1d63c7f4e4dfd7e61a485528404dcc3d
Omar Crosby <omar.crosby@gmail.com>
2025-06-05 19:39:10 -0400

    chore: add commands and core functionality

---
+++ lua/go_task/commands.lua
@@ -0,0 +1,24 @@
+-- lua/go-task/commands.lua
+
+local M = {}
+local task = require("go-task.core")
+
+-- Register Neovim user commands
+function M.setup()
+  vim.api.nvim_create_user_command("GoTaskRun", function(opts)
+    task.run(opts.args)
+  end, {
+    nargs = "?",      -- make argument optional
+    complete = "file" -- or provide a custom completion
+  })
+
+  vim.api.nvim_create_user_command("GoTaskList", function()
+    task.list()
+  end, {})
+
+  vim.api.nvim_create_user_command("GoTaskReload", function()
+    task.reload()
+  end, {})
+end
+
+return M
